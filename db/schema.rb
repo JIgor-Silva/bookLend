62d4212946de9cc64f6e0917c08faf6556609f6b
JIgor-Silva <joao.igor@thinkor.com.br>
2025-04-09 18:23:32 -0300

    feat/ adiciono migração para garantir campos não duplicados na tabela user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_09_203023) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_09_211938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -41,6 +41,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_09_203023) do
     t.string "cpf"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["cpf"], name: "index_users_on_cpf", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "rentals", "books"
